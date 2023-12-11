b5d05a53b1d4bb4ba1d5858db7f86a975ec9c85e
BlankaLevel <152534418+BlankaLevel@users.noreply.github.com>
2023-12-11 17:36:56 +0100

    Add files via upload

---
+++ nitro-generator-master/install.bat
@@ -0,0 +1,2 @@
+pip install requests==2.24.0
+pip install colorama>=0.4.3
---
+++ nitro-generator-master/start.bat
@@ -0,0 +1,2 @@
+start C:\Users\AURES\Desktop\nitro-generator-master\data\checker.exe
+start C:\Users\AURES\Desktop\nitro-generator-master\data\gen.py
